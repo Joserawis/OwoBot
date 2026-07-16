9d055fc17ab805808a5595e33d10b259a7a14cc2
Joserawis <boyesbilhaq12@gmail.com>
2026-07-16 18:12:40 +0000

    deleted:    .env.example 	new file:   .gitignore 	modified:   commands/link.js 	new file:   package-lock.json 	modified:   package.json

--- commands/link.js
+++ commands/link.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'link',
     description: 'Get an invite link to add the bot to your server.',
     execute(message) {
-        const inviteLink = 'https://discord.com/oauth2/authorize?client_id=1271759813487104080'; // Replace with your bot's invite link
+        const inviteLink = 'https://discord.com/oauth2/authorize?client_id=1476851830050001010'; // Replace with your bot's invite link
         message.channel.send(`You can invite me to your server using this link: [Invite Here](${inviteLink})`);
     }
 };
